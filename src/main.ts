ea02c2bbcb80c7cc84e055845daa9bceb4dab625
Val Saven <val.saven@gmail.com>
2019-02-03 04:30:46 +0300

    Migrate to Electron 4 and Typescript

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import 'vuetify/dist/vuetify.min.css';
 
 import App from './App.vue';
 import store from './store';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  iconfont: 'md',
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app');
